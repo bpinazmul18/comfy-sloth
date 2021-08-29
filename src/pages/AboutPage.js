716eeabf3d806bfd917e6f7a69a314b53364dcc3
bpinazmul18 <bpinazmul18@gmail.com>
2021-08-29 22:12:00 +0600

    working on Hero, PageHero, Aboutpage, checkout page & Homepage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,19 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return <main>
+    <PageHero title="about"/>
+    <Wrapper className="page section section-center">
+      <img src={aboutImg} alt="nice desk"/>
+      <article>
+        <div className="title">
+          <h2>our story</h2>
+          <div className="underline"></div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab accusantium amet, consequatur cumque dicta doloremque, ea eos fugit harum, impedit magnam mollitia natus nostrum omnis porro quasi quos reprehenderit vitae voluptates! Aliquid, architecto doloribus facere fugiat laboriosam laborum sequi temporibus. Aliquid dolorem expedita illo impedit iste odit. Deserunt, quas.</p>
+      </article>
+    </Wrapper>
+  </main>
 }
 
 const Wrapper = styled.section`
