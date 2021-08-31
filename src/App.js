e0637d7fb93e2bc254f764390f1dbe0e0e722a8c
bpinazmul18 <bpinazmul18@gmail.com>
2021-08-31 08:05:30 +0600

    working on private route

--- src/App.js
+++ src/App.js
@@ -1,49 +1,62 @@
-import React from 'react'
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
-import {Footer, Navbar, Sidebar} from './components'
-import {About, Cart, Checkout, Error, Home, Products, SingleProduct} from './pages'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { Footer, Navbar, Sidebar } from "./components";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Products,
+  SingleProduct,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-    return (
-        <Router>
-            <Navbar/>
-            <Sidebar/>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-                <Route exact path="/about">
-                    <About/>
-                </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
 
-                <Route exact path="/cart">
-                    <Cart/>
-                </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
 
-                <Route exact path="/products">
-                    <Products/>
-                </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
 
-                <Route exact path="/products/:id" children={<SingleProduct/>}>
-                </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />}></Route>
 
-                <Route exact path="/checkout">
-                    <Checkout/>
-                </Route>
+        <PrivateRoute exact path="/checkout">
+          <Checkout />
+        </PrivateRoute>
 
-                <Route path="*">
-                    <Redirect from='*' to='/404'/>
-                    <Error/>
-                </Route>
+        <Route path="*">
+          <Redirect from="*" to="/404" />
+          <Error />
+        </Route>
 
-                <Route path='/404'>
-                    <Error/>
-                </Route>
-            </Switch>
-            <Footer/>
-        </Router>
-    )
+        <Route path="/404">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
